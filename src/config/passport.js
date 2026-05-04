279489e90980d0a4e2ec6b257b1ff1b3673a8787
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2026-05-04 19:29:34 +0200

    c

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 const { generateUUID } = require('../utils/helpers');
 const crypto = require('crypto');
 
-
+app.use(passport.initialize());
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
