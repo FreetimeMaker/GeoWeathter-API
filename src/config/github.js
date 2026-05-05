442042c396cccb8b8e79a6490eaae7c5d72445c9
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2026-05-05 12:53:29 +0200

    c

--- src/config/github.js
+++ src/config/github.js
@@ -1,4 +1,5 @@
-const fetch = require("node-fetch");
+const https = require("https");
+const querystring = require("querystring");
 
 const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
@@ -8,39 +9,59 @@ function githubLogin(req, res) {
   res.redirect(redirect);
 }
 
-async function githubCallback(req, res) {
-  try {
-    const code = req.query.code;
+function githubCallback(req, res) {
+  const code = req.query.code;
 
-    // 1. Exchange code for access token
-    const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
-      method: "POST",
-      headers: {
-        Accept: "application/json"
-      },
-      body: new URLSearchParams({
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        code
-      })
+  const postData = querystring.stringify({
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    code
+  });
+
+  const options = {
+    hostname: "github.com",
+    path: "/login/oauth/access_token",
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Content-Length": Buffer.byteLength(postData)
+    }
+  };
+
+  const request = https.request(options, (response) => {
+    let data = "";
+
+    response.on("data", (chunk) => {
+      data += chunk;
     });
 
-    const tokenData = await tokenRes.json();
+    response.on("end", () => {
+      try {
+        const json = JSON.parse(data);
 
-    if (!tokenData.access_token) {
-      console.error("GitHub Token Error:", tokenData);
-      return res.status(500).json({ error: "Failed to obtain access token" });
-    }
+        if (!json.access_token) {
+          console.error("GitHub Token Error:", json);
+          return res.status(500).json({ error: "Failed to obtain access token" });
+        }
 
-    const accessToken = tokenData.access_token;
+        const token = json.access_token;
 
-    // 2. Redirect to your app
-    return res.redirect(`geoweather://auth/callback?token=${accessToken}`);
+        return res.redirect(`geoweather://auth/callback?token=${token}`);
+      } catch (err) {
+        console.error("GitHub OAuth parse error:", err);
+        return res.status(500).json({ error: "OAuth failed" });
+      }
+    });
+  });
 
-  } catch (err) {
-    console.error("GitHub OAuth error:", err);
-    res.status(500).json({ message: "OAuth failed", error: err.message });
-  }
+  request.on("error", (err) => {
+    console.error("GitHub OAuth request error:", err);
+    res.status(500).json({ error: "OAuth request failed" });
+  });
+
+  request.write(postData);
+  request.end();
 }
 
 module.exports = { githubLogin, githubCallback };
